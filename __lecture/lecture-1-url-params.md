9f7325cc0170fa6aa8efbb87c6cd132942341d59
Dani Santos <santos.adaniele@gmail.com>
2020-02-18 12:05:47 -0500

    exercise 1.2

--- __lecture/lecture-1-url-params.md
+++ __lecture/lecture-1-url-params.md
@@ -10,16 +10,16 @@ How do you feel about this? Is this DRY?
 // ...
 const app = express();
 
-app.get('/question1', q1)
-app.get('/question2', q2)
-app.get('/question3', q3)
-app.get('/question4', q4)
-app.get('/question5', q5)
-app.get('/question6', q6)
-app.get('/question7', q7)
-app.get('/question8', q8)
-app.get('/question9', q9)
-app.get('/question10', q10)
+app.get("/question1", q1);
+app.get("/question2", q2);
+app.get("/question3", q3);
+app.get("/question4", q4);
+app.get("/question5", q5);
+app.get("/question6", q6);
+app.get("/question7", q7);
+app.get("/question8", q8);
+app.get("/question9", q9);
+app.get("/question10", q10);
 ```
 
 </div><div>
--- server.js
+++ server.js
@@ -1,27 +1,40 @@
-'use strict';
+"use strict";
 
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-const { top50 } = require('./data/top50');
+const express = require("express");
+
+const { top50 } = require("./data/top50");
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.set('view engine', 'ejs');
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 // endpoints here
 
-// handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
+app.get("/top50", (req, res) => {
+  console.log({ top50 });
+  res.render("pages/top50", {
+    title: "Top 50 Songs Streamed on Spotify",
+    top50
+  });
 });
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// handle 404s
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("pages/fourOhFour", {
+    title: "I got nothing",
+    path: req.originalUrl
+  });
+});
+
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT} check http://www.localhost:${PORT}`)
+);
